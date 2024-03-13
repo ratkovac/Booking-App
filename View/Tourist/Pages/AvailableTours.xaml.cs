ac2a83a4e8062bf624f351463a4615b5b0b7737c
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2024-03-13 19:05:33 +0100

    AvailableTours view

---
+++ View/Tourist/Pages/AvailableTours.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace BookingApp.View.Tourist.Pages
+{
+    /// <summary>
+    /// Interaction logic for AvailableTours.xaml
+    /// </summary>
+    public partial class AvailableTours : Page, INotifyPropertyChanged
+    {
+
+        private TourRepository tourRepository;
+        public ObservableCollection<Tour> ListTour { get; set; }
+
+        private int _numberGuest;
+        public int NumberGuests
+        {
+            get => _numberGuest;
+            set
+            {
+                if (value != _numberGuest)
+                {
+                    _numberGuest = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public AvailableTours()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            ListTour = new ObservableCollection<Tour>(tourRepository.GetAll());
+        }
+
+        private void UpdateTourList()
+        {
+            ListTour.Clear();
+            foreach (var tour in tourRepository.GetAll())
+            {
+                ListTour.Add(tour);
+            }
+        }
+
+        public void Update()
+        {
+            UpdateTourList();
+        }
+    }
+}
