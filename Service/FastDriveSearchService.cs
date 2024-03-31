4a909925152e7e5a2ecb1e373e4a0f0e83e0e570
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2024-04-01 00:00:41 +0200

    feat: fastDriveService

--- Service/FastDriveSearchService.cs
+++ Service/FastDriveSearchService.cs
@@ -19,6 +19,167 @@ namespace BookingApp.Service
 {
     public class FastDriveSearchService
     {
-        
+        private User Tourist;
+        private int SelectedLocationId { get; set; }
+        private int AddressId { get; set; }
+        private int DetailedStartAddressId { get; set; }
+        private int DetailedEndAddressId { get; set; }
+
+        public FastDriveSearchService(User user)
+        {
+            Tourist = user;
+        }
+
+        public List<string> GetDistinctCountries()
+        {
+            return new LocationRepository().GetAll()
+                                            .Select(loc => loc.Country)
+                                            .Distinct()
+                                            .OrderBy(c => c)
+                                            .ToList();
+        }
+
+        public List<KeyValuePair<int, string>> GetCitiesByCountry(string country)
+        {
+            return new LocationRepository().GetAll()
+                                            .Where(location => location.Country == country)
+                                            .Select(location => new KeyValuePair<int, string>(location.Id, location.City))
+                                            .Distinct()
+                                            .OrderBy(pair => pair.Value)
+                                            .ToList();
+        }
+
+        public void InputAddressForCity(ComboBox cityComboBox, TextBox streetTextBox)
+        {
+            InputAddress(cityComboBox, streetTextBox);
+            if (cityComboBox.SelectedItem is KeyValuePair<int, string> selectedCity)
+            {
+                AddressId = selectedCity.Key;
+            }
+        }
+
+        private void InputAddress(ComboBox cityComboBox, TextBox streetTextBox)
+        {
+            if (cityComboBox.SelectedItem != null)
+            {
+                var selectedCity = (KeyValuePair<int, string>)cityComboBox.SelectedItem;
+                AddressRepository addressRepository = new AddressRepository();
+
+                string input = streetTextBox.Text.Trim();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    streetTextBox.Text = "";
+                    return;
+                }
+
+                string[] parts = input.Split(',');
+                if (parts.Length != 2)
+                {
+                    MessageBox.Show("Neispravan format unosa. Molimo unesite ulicu i broj odvojene zarezom.");
+                    return;
+                }
+
+                string streetName = parts[0].Trim();
+                string streetNumber = parts[1].Trim();
+
+                var streets = addressRepository.GetAll()
+                    .Where(address => address.LocationId == selectedCity.Key && address.Street.Equals(streetName, StringComparison.OrdinalIgnoreCase) && address.Number.Equals(streetNumber, StringComparison.OrdinalIgnoreCase))
+                    .Select(address => address.Street)
+                    .Distinct()
+                    .OrderBy(street => street)
+                    .ToList();
+
+                streetTextBox.Text = string.Join(Environment.NewLine, streets);
+            }
+            else
+            {
+                streetTextBox.Text = "";
+            }
+        }
+
+        public void SetDetailedAddressId(TextBox streetTextBox, bool isStartAddress)
+        {
+            string input = streetTextBox.Text.Trim();
+            string[] parts = input.Split(',');
+
+            string streetName = parts[0].Trim();
+            string streetNumber = parts[1].Trim();
+
+            AddressRepository addressRepository = new AddressRepository();
+            var address = addressRepository.GetByAddress(streetName, streetNumber);
+            if (address != null)
+            {
+                if (isStartAddress) DetailedStartAddressId = address.Id;
+                else DetailedEndAddressId = address.Id;
+            }
+        }
+
+        public DateTime CreateDateTimeFromSelections(DateTime? selectedDate, string hourText, ComboBoxItem selectedMinuteItem)
+        {
+            if (selectedDate.HasValue &&
+                !string.IsNullOrEmpty(hourText) &&
+                int.TryParse(hourText, out int hour) &&
+                hour >= 0 && hour < 24 &&
+                selectedMinuteItem != null)
+            {
+                int minute = int.Parse(selectedMinuteItem.Content.ToString());
+
+                return new DateTime(selectedDate.Value.Year, selectedDate.Value.Month, selectedDate.Value.Day, hour, minute, 0);
+            }
+            else
+            {
+                throw new ArgumentException("Molimo unesite validan datum i sat.");
+            }
+        }
+
+        public void Reservation(TextBox startStreetTextBox, TextBox endStreetTextBox, DateTime departure)
+        {
+            SetDetailedAddressId(startStreetTextBox, true);
+            SetDetailedAddressId(endStreetTextBox, false);
+            if (DetailedStartAddressId == 0)
+            {
+                DetailedStartAddressId = AddNewAddress(startStreetTextBox.Text.Trim());
+            }
+
+            if (DetailedEndAddressId == 0)
+            {
+                DetailedEndAddressId = AddNewAddress(endStreetTextBox.Text.Trim());
+            }
+
+            Drive drive = new Drive(DetailedStartAddressId, DetailedEndAddressId, departure, Tourist.Id, 2, 0);
+            DriveRepository driveRepository = new DriveRepository();
+            driveRepository.Save(drive);
+            MessageBox.Show("Rezervacija uspješna");
+        }
+
+        private int AddNewAddress(string address)
+        {
+            string[] parts = address.Split(',');
+
+            if (parts.Length != 2)
+            {
+                MessageBox.Show("Neispravan format unosa. Molimo unesite ulicu i broj odvojene zarezom.");
+                return 0;
+            }
+
+            string streetName = parts[0].Trim();
+            string streetNumber = parts[1].Trim();
+
+            AddressRepository addressRepository = new AddressRepository();
+
+            Address newAddress = new Address
+            {
+                Id = addressRepository.NextId(),
+                LocationId = SelectedLocationId,
+                Street = streetName,
+                Number = streetNumber
+            };
+
+            addressRepository.Save(newAddress);
+
+            return newAddress.Id;
+        }
+
     }
 }
--- View/Tourist/Pages/FastDriveSearch.xaml.cs
+++ View/Tourist/Pages/FastDriveSearch.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BookingApp.Model;
 using BookingApp.Repository;
+using BookingApp.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,74 +18,56 @@ using System.Windows.Shapes;
 
 namespace BookingApp.View.Tourist.Pages
 {
-    /// <summary>
-    /// Interaction logic for FastDriveSearch.xaml
-    /// </summary>
     public partial class FastDriveSearch : Page
     {
+        private FastDriveSearchService fastDriveSearchService;
         public User Tourist;
-        public int AddressId { get; set; }
-        public int DetailedStartAddressId { get; set; }
-        public int DetailedEndAddressId { get; set; }
         private int SelectedLocationId { get; set; }
 
         public FastDriveSearch(User user)
         {
             InitializeComponent();
             Tourist = user;
-            InputCountries(CountryComboBox);
+            fastDriveSearchService = new FastDriveSearchService(Tourist);
+            InputCountries();
         }
 
-        private void InputCountries(ComboBox comboBox)
+        private void InputCountries()
         {
-            var countries = GetDistinctCountries();
-            UpdateComboBoxItems(comboBox, countries);
+            var countries = fastDriveSearchService.GetDistinctCountries();
+            UpdateComboBoxItems(CountryComboBox, countries);
         }
 
-        private List<string> GetDistinctCountries()
+        private void CountryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            return new LocationRepository().GetAll()
-                                            .Select(loc => loc.Country)
-                                            .Distinct()
-                                            .OrderBy(c => c)
-                                            .ToList();
+            InputCities();
+        }
+
+        private void InputCities()
+        {
+            string selectedCountry = CountryComboBox.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedCountry))
+            {
+                ClearComboBox(CityComboBox);
+                return;
+            }
+
+            var cities = fastDriveSearchService.GetCitiesByCountry(selectedCountry);
+
+            if (cities.Count > 0)
+            {
+                UpdateCityComboBox(CityComboBox, cities);
+            }
+            else
+            {
+                ClearComboBox(CityComboBox);
+            }
         }
 
         private void UpdateComboBoxItems(ComboBox comboBox, List<string> items)
         {
             comboBox.ItemsSource = items;
-        }
-
-        private void InputCities(ComboBox countryComboBox, ComboBox cityComboBox)
-        {
-            string selectedCountry = countryComboBox.SelectedItem?.ToString();
-
-            if (string.IsNullOrEmpty(selectedCountry))
-            {
-                ClearComboBox(cityComboBox);
-                return;
-            }
-
-            var cities = GetCitiesByCountry(selectedCountry);
-
-            if (cities.Count > 0)
-            {
-                UpdateCityComboBox(cityComboBox, cities);
-            }
-            else
-            {
-                ClearComboBox(cityComboBox);
-            }
-        }
-
-        private List<KeyValuePair<int, string>> GetCitiesByCountry(string country)
-        {
-            return new LocationRepository().GetAll()
-                                            .Where(location => location.Country == country)
-                                            .Select(location => new KeyValuePair<int, string>(location.Id, location.City))
-                                            .Distinct()
-                                            .OrderBy(pair => pair.Value)
-                                            .ToList();
         }
 
         private void UpdateCityComboBox(ComboBox cityComboBox, List<KeyValuePair<int, string>> items)
@@ -107,156 +90,20 @@ namespace BookingApp.View.Tourist.Pages
             comboBox.ItemsSource = null;
         }
 
-        private void InputAddress(ComboBox cityComboBox, TextBox streetTextBox)
-        {
-            if (cityComboBox.SelectedItem != null)
-            {
-                var selectedCity = (KeyValuePair<int, string>)cityComboBox.SelectedItem;
-                AddressRepository addressRepository = new AddressRepository();
-
-                string input = streetTextBox.Text.Trim();
-
-                if (string.IsNullOrEmpty(input))
-                {
-                    streetTextBox.Text = "";
-                    return;
-                }
-
-                string[] parts = input.Split(',');
-                if (parts.Length != 2)
-                {
-                    MessageBox.Show("Neispravan format unosa. Molimo unesite ulicu i broj odvojene zarezom.");
-                    return;
-                }
-
-                string streetName = parts[0].Trim();
-                string streetNumber = parts[1].Trim();
-
-                var streets = addressRepository.GetAll()
-                    .Where(address => address.LocationId == selectedCity.Key && address.Street.Equals(streetName, StringComparison.OrdinalIgnoreCase) && address.Number.Equals(streetNumber, StringComparison.OrdinalIgnoreCase))
-                    .Select(address => address.Street)
-                    .Distinct()
-                    .OrderBy(street => street)
-                    .ToList();
-
-                streetTextBox.Text = string.Join(Environment.NewLine, streets);
-            }
-            else
-            {
-                streetTextBox.Text = "";
-            }
-        }
-
-
         private void InputAddressForCity(ComboBox cityComboBox, TextBox streetTextBox)
         {
-            InputAddress(cityComboBox, streetTextBox);
-            if (cityComboBox.SelectedItem is KeyValuePair<int, string> selectedCity)
-            {
-                AddressId = selectedCity.Key;
-            }
-        }
-
-        private void SetDetailedAddressId(TextBox streetTextBox, bool isStartAddress)
-        {
-            string input = streetTextBox.Text.Trim();
-
-            string[] parts = input.Split(',');
-
-            string streetName = parts[0].Trim();
-            string streetNumber = parts[1].Trim();
-
-            AddressRepository addressRepository = new AddressRepository();
-            var address = addressRepository.GetByAddress(streetName, streetNumber);
-            if (address != null)
-            {
-                if (isStartAddress) DetailedStartAddressId = address.Id;
-                else DetailedEndAddressId = address.Id;
-            }
+            fastDriveSearchService.InputAddressForCity(cityComboBox, streetTextBox);
         }
 
         private DateTime CreateDateTimeFromSelections()
         {
-            if (dateDp.SelectedDate.HasValue &&
-                !string.IsNullOrEmpty(hourTextBox.Text) &&
-                int.TryParse(hourTextBox.Text, out int hour) &&
-                hour >= 0 && hour < 24 &&
-                minuteComboBox.SelectedItem is ComboBoxItem selectedMinuteItem)
-            {
-                int minute = int.Parse(selectedMinuteItem.Content.ToString());
-
-                return new DateTime(dateDp.SelectedDate.Value.Year, dateDp.SelectedDate.Value.Month, dateDp.SelectedDate.Value.Day, hour, minute, 0);
-            }
-            else
-            {
-                MessageBox.Show("Molimo unesite validan datum i sat.");
-                return DateTime.MinValue;
-            }
-        }
-
-        private bool AreAllCriteriaMet()
-        {
-            return dateDp.SelectedDate != null &&
-                   CountryComboBox.SelectedItem != null &&
-                   CityComboBox.SelectedItem != null &&
-                   startStreetTextBox != null &&
-                   endStreetTextBox != null &&
-                   minuteComboBox.SelectedItem != null;
+            return fastDriveSearchService.CreateDateTimeFromSelections(dateDp.SelectedDate, hourTextBox.Text, minuteComboBox.SelectedItem as ComboBoxItem);
         }
 
         private void Reservation_Click(object sender, RoutedEventArgs e)
         {
             DateTime departure = CreateDateTimeFromSelections();
-
-            SetDetailedAddressId(startStreetTextBox, true);
-            SetDetailedAddressId(endStreetTextBox, false);
-            if (DetailedStartAddressId == 0)
-            {
-                DetailedStartAddressId = AddNewAddress(startStreetTextBox.Text.Trim());
-            }
-
-            if (DetailedEndAddressId == 0)
-            {
-                DetailedEndAddressId = AddNewAddress(endStreetTextBox.Text.Trim());
-            }
-
-            Drive drive = new(DetailedStartAddressId, DetailedEndAddressId, departure, Tourist.Id, 2, 0);
-            DriveRepository driveRepository = new DriveRepository();
-            driveRepository.Save(drive);
-            MessageBox.Show("Rezervacija uspješna");
-        }
-
-        private int AddNewAddress(string address)
-        {
-            string[] parts = address.Split(',');
-
-            if (parts.Length != 2)
-            {
-                MessageBox.Show("Neispravan format unosa. Molimo unesite ulicu i broj odvojene zarezom.");
-                return 0;
-            }
-
-            string streetName = parts[0].Trim();
-            string streetNumber = parts[1].Trim();
-
-            AddressRepository addressRepository = new AddressRepository();
-
-            Address newAddress = new Address
-            {
-                Id = addressRepository.NextId(),
-                LocationId = SelectedLocationId,
-                Street = streetName,
-                Number = streetNumber
-            };
-
-            addressRepository.Save(newAddress);
-
-            return newAddress.Id;
-        }
-
-        private void CountryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            InputCities(CountryComboBox, CityComboBox);
+            fastDriveSearchService.Reservation(startStreetTextBox, endStreetTextBox, departure);
         }
 
         private void CityComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
