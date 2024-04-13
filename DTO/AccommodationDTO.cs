2a4277a866bb0071013d8a8eed3445a2b40b8f0a
Pejanovic <pejanovic2002@gmail.com>
2024-04-13 23:35:26 +0200

    fix:fixing errors

--- DTO/AccommodationDTO.cs
+++ DTO/AccommodationDTO.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static BookingApp.Model.AccommodationTypeEnum;
-using AccommodationType = BookingApp.Model.AccommodationType;
+
 
 namespace BookingApp.DTO
 {
