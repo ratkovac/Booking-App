2d38c6a733a7f1a4c349dfd7c87d0dffd3cf774f
Nikola Letvenčuk <letvencuk.ra93.2021@uns.ac.rs>
2024-04-13 12:52:53 +0200

    fix: Taximeter

---
+++ Model/DriveStats.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model
+{
+    class DriveStats
+    {
+        public int DriveId { get; set; }
+
+        public Drive Drive { get; set; }
+
+        public DriveDriven Stats { get; set; }
+
+        public DriveStats()
+        {
+        }
+        public DriveStats(int driveId, Drive drive, DriveDriven stats)
+        {
+            DriveId = driveId;
+            Drive = drive;
+            Stats = stats;
+        }
+
+    }
+}
