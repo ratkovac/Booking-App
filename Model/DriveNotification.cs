bd3f128317ef7790ebc5c70ce321491afaded198
Nikola Letvenčuk <letvencuk.ra93.2021@uns.ac.rs>
2024-04-15 10:52:00 +0200

    Fix: Notifications

---
+++ Model/DriveNotification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookingApp.Model
+{
+    public class DriveNotification
+    {
+        public int Id { get; set; }
+
+        public string Caption { get; set; }
+
+        public string Text { get; set; }
+        
+        public DriveNotification(string caption, string text)
+        {
+            this.Caption = caption;
+            this.Text = text;
+        }
+        public string[] ToCSV()
+        {
+            return new string[]
+            {
+                Id.ToString(),
+                Caption,
+                Text
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Caption: {Caption}, Text: {Text}";
+        }
+
+        public void FromCSV(string[] values)
+        {
+            if (values.Length != 3)
+            {
+                throw new ArgumentException("Neispravan format CSV podataka.");
+            }
+
+            Id = Convert.ToInt32(values[0]);
+            Caption = values[1];
+            Text = values[2];
+        }
+    }
+}
---
+++ View/Driver/NotificationWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using BookingApp.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace BookingApp.View.Driver
+{
+    public partial class NotificationWindow : Window
+    {
+        private ObservableCollection<string> notificationCaptions = new ObservableCollection<string>();
+
+        private ObservableCollection<DriveNotification> notifications = new ObservableCollection<DriveNotification>();
+
+        private User LoggedDriver;
+  
+        public NotificationWindow(User driver)
+        {
+            InitializeComponent();
+
+            NotificationList.ItemsSource = notificationCaptions;
+            LoggedDriver = driver;
+
+            DriveNotification newNotification1 = new DriveNotification("Naslov1", "Tekst1");
+            notifications.Add(newNotification1);
+            notificationCaptions.Add(newNotification1.Caption);
+            DriveNotification newNotification2 = new DriveNotification("Naslov2", "Tekst2");
+            notifications.Add(newNotification2);
+            notificationCaptions.Add(newNotification2.Caption);
+            DriveNotification newNotification3 = new DriveNotification("Naslov3", "Tekst3");
+            notifications.Add(newNotification3);
+            notificationCaptions.Add(newNotification3.Caption);
+            DriveNotification newNotification4 = new DriveNotification("Naslov4", "Tekst4");
+            notifications.Add(newNotification4);
+            notificationCaptions.Add(newNotification4.Caption);
+        }
+
+        private void AddNotification_Click(object sender, RoutedEventArgs e)
+        {
+            DriveNotification newNotification = new DriveNotification("Naslov", "Tekst");
+            notifications.Add(newNotification); 
+            notificationCaptions.Add(newNotification.Caption); 
+        }
+
+        private void RemoveNotification_Click(object sender, RoutedEventArgs e)
+        {
+            if (NotificationList.SelectedIndex != -1)
+            {
+                int selectedIndex = NotificationList.SelectedIndex;
+                notifications.RemoveAt(selectedIndex);
+                notificationCaptions.RemoveAt(selectedIndex);
+            }
+        }
+
+        private void NotificationList_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (NotificationList.SelectedItem != null)
+            {
+                if (NotificationList.SelectedIndex != -1)
+                {
+                    int selectedIndex = NotificationList.SelectedIndex;
+                    MessageDisplay.Text = notifications[selectedIndex].Caption;
+                }
+            }
+        }
+
+        private void FastDrivesCheck()
+        {
+            //_fastDrivesRepository = new FastDrivesRepository();
+            //_fastDrivesRepository.checkFastDrivesForDriversLocation();
+
+
+        }
+    }
+}
