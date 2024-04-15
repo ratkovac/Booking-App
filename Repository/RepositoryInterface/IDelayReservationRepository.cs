883d300464b8c0ae7e2c0e80a5c78264cf876c34
Pejanovic <pejanovic2002@gmail.com>
2024-04-15 21:38:44 +0200

    add: record-delay-Reservation-in-repository

---
+++ Repository/RepositoryInterface/IDelayReservationRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookingApp.Model;
+using CLI.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repository.RepositoryInterface
+{
+    public interface IDelayReservationRepository : IGenericRepository<DelayReservation, int>
+    {
+        public void Subscribe(IObserver observer)
+        {
+
+        }
+    }
+}
