5016839191c0948593c3212fd72020189d94c871
LazarMitar <laki26062002@gmail.com>
2024-05-12 14:00:05 +0200

    Feature renovations

---
+++ View/Owner/AllRenovations.xaml.cs
@@ -0,0 +1,19 @@
+﻿using BookingApp.View.ViewModel.Owner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookingApp.View.Owner
+{
+    public partial class AllRenovations : Window
+    {
+        public AllRenovations(AllRenovationsViewModel allRenovationsViewModel)
+        {
+            InitializeComponent();
+            this.DataContext = allRenovationsViewModel;
+        }
+    }
+}
